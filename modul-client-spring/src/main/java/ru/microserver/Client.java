8e03094f7cd4a1f9468c7eddf0e450ac29b4033a
DmitriyV <dima1999-91@mail.ru>
2023-11-18 23:28:47 +0300

    создан клиент и соединен с сервером

---
+++ modul-client-spring/src/main/java/ru/microserver/Client.java
@@ -0,0 +1,18 @@
+package ru.microserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+
+
+public class Client {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client.class, args);
+
+    }
+}
