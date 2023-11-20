abf3327975735a5f2fb5e987fbfc145fecdb2363
DmitriyV <dima1999-91@mail.ru>
2023-11-20 23:42:22 +0300

    добавление api Gateway

---
+++ module-API-Gateway/src/main/java/ru/microserver/APIStarter.java
@@ -0,0 +1,15 @@
+package ru.microserver;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class APIStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APIStarter.class, args);
+
+    }
+}
